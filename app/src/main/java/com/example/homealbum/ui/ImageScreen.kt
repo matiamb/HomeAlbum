cf0744a3c9d4044a60ba3e7b2899e48c5e424c3b
Mati Ambrosi <matiambrosi@gmail.com>
2026-07-21 18:18:51 -0300

    Double tap and pinch to zoom functionality added and working

--- app/src/main/java/com/example/homealbum/ui/ImageScreen.kt
+++ app/src/main/java/com/example/homealbum/ui/ImageScreen.kt
@@ -1,9 +1,17 @@
 package com.example.homealbum.ui
 
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.animateOffsetAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.awaitEachGesture
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.calculatePan
+import androidx.compose.foundation.gestures.calculateZoom
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyRow
@@ -13,14 +21,18 @@ import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.positionChanged
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -52,52 +64,85 @@ fun ImageRoll(
         initialPage = initialPageIndex,
         pageCount = {uiState.value.photoList.size}
     )
-
-//    LazyRow(
-//        modifier = modifier.fillMaxSize()
-//    ) {
-////        items(uiState.value.photoList){item ->
-////            AsyncImage(
-////                model = item,
-////                contentDescription = "",
-////                modifier = Modifier.fillMaxWidth()
-////            )
-////        }
-//        items(imageList){item ->
-//            Image(
-//                painter = painterResource(item.photoRes),
-//                contentDescription = "",
-//                modifier.fillMaxSize(),
-//                contentScale = ContentScale.FillWidth
-//            )
-//        }
-//    }
+    var scale by remember { mutableFloatStateOf(1f)}
+    var offset by remember { mutableStateOf(Offset.Zero) }
+    var isZoomed by remember { mutableStateOf(false) }
     HorizontalPager(
         state = pagerState,
-        modifier = modifier.fillMaxSize().background(color = MaterialTheme.colorScheme.inverseOnSurface)
+        userScrollEnabled = !isZoomed,
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colorScheme.inverseOnSurface)
+            .pointerInput(Unit) {
+                //Este detect transform gestures va aca por que si no sobreescribe el gesto de swipe para pasar de foto
+                //del pager, entonces si lo pongo aca funciona bien
+                detectTransformGestures { _, pan, zoom, _ ->
+                    //el coerceIn lo que hace es limitar el valor entre los limites que yo le pongo
+                    scale = (scale * zoom).coerceIn(1f, 3f)
+                    //esto es para calcular cuanto puedo desplazar la foto una vez tenga el zoom hecho
+                    val maxPanX = (size.width * (scale - 1)) / 2
+                    val maxPanY = (size.height * (scale - 1)) / 2
+                    offset = Offset(
+                        x = (offset.x + pan.x * scale).coerceIn(-maxPanX, maxPanX),
+                        y = (offset.y + pan.y * scale).coerceIn(-maxPanY, maxPanY)
+                    )
+                }
+            }
     ) { page ->
         val uri = uiState.value.photoList[page]
-        var isZoomed by remember { mutableStateOf(false) }
-        val scale by animateFloatAsState(
-            targetValue = if (isZoomed) 3f else 1f,
-            label = "zoom_animation"
-        )
+//        var scale by remember { mutableFloatStateOf(1f)}
+//        var offset by remember { mutableStateOf(Offset.Zero) }
+// Esto es para que haga una animacion cada vez que haya zoom
+        val animatedScale by animateFloatAsState(targetValue = scale, label = "scale")
+        val animatedOffset by animateOffsetAsState(targetValue = offset, label = "offset")
+        //Este launched effect es para que protegerme de un bucle infinito, es decir que
+        //se va a activar esta parte del codigo cuando el scale sufra algun cambio
+        LaunchedEffect(scale) {
+               isZoomed = scale > 1f
+        }
         AsyncImage(
             model = uri,
             contentDescription = "",
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .fillMaxSize()
+                //aca le digo a la gpu que animaciones hacer en los cambio de escala
                 .graphicsLayer(
-                    scaleX = scale,
-                    scaleY = scale
+                    scaleX = animatedScale,
+                    scaleY = animatedScale,
+                    translationX = animatedOffset.x,
+                    translationY = animatedOffset.y
                 )
-                .pointerInput(Unit){
+                //El pointer input es lo que detecta los toques en la pantalla, en este caso
+                //dentro uso el detect tap gestures para que detecte el double tap
+                //luego dentro tengo la logica para los limites de pantalla
+                .pointerInput(Unit) {
                     detectTapGestures(
                         onDoubleTap = {
-                            isZoomed = !isZoomed
+                            if (scale > 1f) {
+                                scale = 1f
+                                offset = Offset.Zero
+                            } else {
+                                scale = 2f
+                                val targetOffsetX = (size.width / 2 - it.x) * scale
+                                val targetOffsetY = (size.height / 2 - it.y) * scale
+                                offset = Offset(targetOffsetX, targetOffsetY)
+                            }
                         }
                     )
-                },
-            contentScale = ContentScale.Fit
+                }
+//                .pointerInput(Unit) {
+//                    //Esto no termina de funcionar, el pinch to zoom solo funciona si hago el doble tap antes
+//                    detectTransformGestures { _, pan, zoom, _ ->
+//                        scale = (scale * zoom).coerceIn(1f, 3f)
+//                        val maxPanX = (size.width * (scale - 1)) / 2
+//                        val maxPanY = (size.height * (scale - 1)) / 2
+//                        offset = Offset(
+//                            x = (offset.x + pan.x * scale).coerceIn(-maxPanX, maxPanX),
+//                            y = (offset.y + pan.y * scale).coerceIn(-maxPanY, maxPanY)
+//                        )
+//                    }
+//                },
+            ,contentScale = ContentScale.Fit
         )
     }
 }
