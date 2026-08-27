cb4da077f92034118017efbf994d3af1b75626a4
Mati Ambrosi <matiambrosi@gmail.com>
2026-08-27 16:30:47 -0300

    Changed version code and name to target new release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.homealbum"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0-beta.1"
+        versionCode = 2
+        versionName = "0.2.0-beta.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
